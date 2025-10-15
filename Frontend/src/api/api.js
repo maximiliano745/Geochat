518d2d102c5db688586712bbfbdc0bb0fdc1bfe2
Maximiliano Armando Altamirano <44042402+maximiliano745@users.noreply.github.com>
2025-10-15 16:03:22 +0000

    inicio

---
+++ Frontend/src/api/api.js
@@ -0,0 +1,11 @@
+// GeoChat/Frontend/src/api/api.js
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default api
